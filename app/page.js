40498404e13f9d963f1503fcac080e87d4f1e9e1
seishiiin <gabinhalloss@gmail.com>
2025-04-08 20:30:54 +0200

    v.0.1: hero section

--- app/page.js
+++ app/page.js
@@ -3,10 +3,16 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className={"w-screen flex flex-col items-center bg-white justify-start"}>
+        <div className={"w-screen flex flex-col items-center bg-white justify-start px-10"}>
             <Navbar />
             
             <Image src={"/assets/apple-ring.png"} alt={"Apple Ring"} width={300} height={300} className={"w-1/2 h-auto mt-10"} />
+
+            <div className={"w-4/6 flex flex-col items-center justify-center"}>
+                <h1 className={" flex justify-start items-start gap-1 font-bold text-4xl"}><Image src={"/assets/apple.svg"} alt={"Apple"} width={30} height={30} className={"w-8 h-8"} /> Ring</h1>
+                <p className={"text-gray-500 text-sm"}>Plus qu’un bijou. Une révolution.</p>
+                <button className={"bg-[#306fdb] text-white rounded-full px-6 py-2 text-md mt-5"}>Acheter maintenant</button>
+            </div>
         </div>
     )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar() {
     return (
-        <div className={"w-4/6 flex justify-between items-center border-b border-[#d6d6d6] pt-10 pb-3 px-1"}>
+        <div className={"sticky top-0 w-4/6 backdrop-blur-md flex justify-between items-center border-b border-[#d6d6d6] pt-10 pb-3 px-1"}>
             <h1 className={"font-bold text-xl"}>Apple Ring</h1>
 
-            <button className={"bg-[#306fdb] text-white rounded-full px-3 py-px text-sm"}>Buy</button>
+            <button className={"bg-[#306fdb] text-white rounded-full px-3 py-px text-sm"}>Acheter</button>
         </div>
     )
 }
